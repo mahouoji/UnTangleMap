bcf936debe2220df65da578f63273f50fe082614
Hang Dong <hang@Hangs-MBP.fios-router.home>
2020-03-30 12:35:39 -0400

    parser for BDLP

--- ext/DBLPParser/src/parse_author_conf.py
+++ ext/DBLPParser/src/parse_author_conf.py
@@ -18,7 +18,9 @@ def main(argv):
             else:
                 key = row[0]
                 author = row[1]
+                year = int(row[2])
                 line_count += 1
+                if year < 2010 or year > 2020: continue
                 if not key.startswith('conf'):
                     continue
                 conf = key.split('/', 3)[1].strip()
@@ -31,14 +33,31 @@ def main(argv):
                 confs[conf] += 1
                 # update author dict
                 if author not in stat:
-                    stat[author] = dict()
+                    stat[author] = {'total': 0}
                 if conf not in stat[author]:
                     stat[author][conf] = 0
                 stat[author][conf] += 1
+                stat[author]['total'] += 1
     # sort conference by count
     confs = {k: v for k, v in sorted(confs.items(), key=lambda item: item[1], reverse=True)}
+    stat = {k: v for k, v in sorted(stat.items(), key=lambda item: item[1]['total'], reverse=True)}
+    for k in stat:
+        stat[k] = {k: v for k, v in sorted(stat[k].items(), key=lambda item: item[1], reverse=True)}
+
+    confs_top = {}
+    stat_top = {k: v for k, v in sorted(stat.items(), key=lambda item: item[1]['total'], reverse=True)[:3500]}
+    for k in stat_top:
+        for ck, cv in stat_top[k].items():
+            if ck not in confs_top:
+                confs_top[ck] = 0
+            confs_top[ck] += cv
+    confs_top = {k: v for k, v in sorted(confs_top.items(), key=lambda item: item[1], reverse=True)}
+
+    print('total first authors:', len(stat))
     json.dump(confs, open('../dataset/confs.json', 'w'), indent=2)
     json.dump(stat, open('../dataset/authors.json', 'w'), indent=2)
+    json.dump(confs_top, open('../dataset/confs_top3500.json', 'w'), indent=2)
+    json.dump(stat_top, open('../dataset/authors_top3500.json', 'w'), indent=2)
 
 if __name__ == '__main__':
     main(sys.argv)
