1e53847ec0a7d6ec632de684f0feb081942dc92f
Hang Dong <hang@Hangs-MBP.fios-router.home>
2020-04-10 00:07:24 -0400

    compute utility update (tbc)

--- src/js/untanglemap_layout.js
+++ src/js/untanglemap_layout.js
@@ -10,14 +10,18 @@
         initLabelLayout: function(data, center) {
             var self = this;
             center = center || HexCord(0, 0);
-    
+            self.data = data;
+
+            // TODO: sort labels
             //let label = self.getFirstLabel(data.labels);
             //self.addLabel(label.name, center);
-            let labelNames = data.labels.map(l => l.name);
+            let labelNames = self.data.labels.map(l => l.name);
             self.addLabel(labelNames[0], center);
             self.addLabel(labelNames[1], center.getNeighbors()[0]);
 
+            // without layout algorithm
             for (let i = 2; i < labelNames.length; i++) {
+                self.placeLable(labelNames);
                 //console.log(self.labelMap.cand);
                 for (let key in self.labelMap.cand) {
                     if (!(key in self.labelMap.in) && self.labelMap.cand[key].cnt >= 2) {
@@ -26,6 +30,7 @@
                     }
                 }
             }
+            // compute layout
             /*while(labelNames.length > 0) {
                 name = labelNames
                 Map.addLabel(name, cord);
@@ -55,6 +60,59 @@
             return $.map(this.labelMap.faces, function(value, key) { return value });
         },
 
+        placeLable: function(labels) {
+            var self = this;
+            let maxUtil = -10000.0;
+            let maxLabel = '';
+            $.each(self.labelMap.cand, function(key, _){
+                //valid slots
+                if (!(key in self.labelMap.in) && self.labelMap.cand[key].cnt >= 2) {
+                    labels.forEach(name=>{
+                        let cord = self.labelMap.cand[key].cord;
+                        let util = self.getUpdatedUtility(name, cord);
+                        if (maxUtil < util) {
+                            maxUtil = util;
+                            maxLabel = name;
+                        }
+                    })
+                }
+            });
+            console.log(maxLabel);
+            console.log(maxUtil);
+            return maxLabel;
+        },
+
+        // compute utility when putting label at cord
+        getUpdatedUtility: function(labelName, cord) {
+            var self = this;
+            let id = self.data.labelIndex[labelName];
+            let edgeCorr = 0.0;
+            let edgeCnt = 0;
+            let triCorr = 0.0;
+            let triCnt = 0;
+            let neighbors = cord.getNeighbors();
+            neighbors.forEach((ncord, i) => {
+                let nkey = ncord.toString();
+                if (nkey in self.labelMap.in) {
+                    // update edge corr
+                    let nid = self.labelMap.in[nkey].index;
+                    edgeCorr += self.data.corr[self.utility.method][nid][id];
+                    edgeCnt += 1;
+                    // update face corr
+                    let nnkey = neighbors[(i + 1) % 6].toString();
+                    if (nnkey in self.labelMap.in) {
+                        let nnid = self.labelMap.in[nnkey].index;
+                        triCorr += (self.data.corr[self.utility.method][nid][id]
+                                + self.data.corr[self.utility.method][nid][nnid]
+                                + self.data.corr[self.utility.method][nnid][id]) / 3.0;
+                        triCnt += 1;
+                    }
+                }
+            });
+            return self.utility.alpha * (self.utility.edgeCorr + edgeCorr) / (self.utility.edgeCnt + edgeCnt)
+                + (1 - self.utility.alpha) * (self.utility.triCorr + triCorr) / (self.utility.triCnt + triCnt);
+        },
+
         addLabel: function(labelName, cord) {
             var self = this;
             //TODO: check validity
@@ -68,6 +126,7 @@
             // add to map
             self.labelMap.in[key] = {
                 'name': labelName,
+                'index': self.data.labelIndex[labelName],
                 'cord': cord
             };
             // update neighbors
@@ -117,29 +176,39 @@
                 if (self.labelMap.cand[nkey].cnt === 0) {
                     delete self.labelMap.cand[nkey];
                 }
-            })
+            });
             faces.forEach(fcord => {
                 let fkey = fcord.toString();
                 if (fkey in self.labelMap.faces) {
                     delete self.labelMap.faces[fkey];
                 }
-            })
+            });
             console.log(self.labelMap);
-        }
-        
+        },
+
     };
     
     UnTangleMap.init = function () {
         var self = this;
         // data
-        self.data = {labels: [], items: []};
+        self.data = {};
         // label map
         self.labelMap = {
             in: {},
             cand: {},
             faces: {}
         };
-        self.labels = {};
+        self.utility = {
+            // records
+            edgeCorr: 0.0,
+            edgeCnt: 0,
+            triCorr: 0.0,
+            triCnt: 0,
+            // settings
+            alpha: 0.5,
+            method: 'spearman'
+        }
+
     }
     
     UnTangleMap.init.prototype = UnTangleMap.prototype;
