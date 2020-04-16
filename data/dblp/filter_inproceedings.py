705ca8cb7196ee0298682886b3e1c0130a11d476
Hang Dong <hang@Hangs-MBP.fios-router.home>
2020-04-15 23:42:19 -0400

    get top author-conference

--- data/dblp/filter_inproceedings.py
+++ data/dblp/filter_inproceedings.py
@@ -30,6 +30,7 @@ def main(argv):
     print('%d conferences with hindex >= %d' % (len(top_confs), HINDEX))
     
     confs = {}
+    author_rec = {}
 
     with open(INPROCEEDINGS_FILENAME) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -53,9 +54,47 @@ def main(argv):
                         if key not in confs:
                             confs[key] = 0
                         confs[key] += 1
+                    else:
+                        continue
+                    #parse authors
+                    author = author.split('::')
+                    key = top_confs[key]['abbr']
+                    for a in author:
+                        if a not in author_rec:
+                            author_rec[a] = {
+                                'name': a,
+                                'confs': {},
+                                'total': 0
+                            }
+                        if key not in author_rec[a]['confs']:
+                            author_rec[a]['confs'][key] = 0
+                        author_rec[a]['confs'][key] += 1
+                        author_rec[a]['total'] += 1
+
     for key in top_confs:
         if key not in confs:
             print('%s | %s | %d' % (key, top_confs[key]['fullname'], top_confs[key]['hindex']))
+    print('%d conference found from file' % len(confs))
+
+    with open('./data/authorconf.csv', 'w') as f:
+        csv_writer = csv.writer(f)
+        conf_keys = [top_confs[key]['abbr'] for key in top_confs]
+        csv_writer.writerow(['author'] + conf_keys)
+        topauthors = {k: v for k, v in sorted(author_rec.items(), key=lambda item: item[1]['total'],reverse=True)}
+        cnt = 0
+        last_total = 0
+        for author in topauthors:
+            cnt += 1
+            if cnt > 3000 and author_rec[author]['total'] < last_total:
+                break
+            last_total = author_rec[author]['total']
+            confstat = []
+            for conf in conf_keys:
+                if conf in author_rec[author]['confs']:
+                    confstat.append(author_rec[author]['confs'][conf])
+                else:
+                    confstat.append(0)
+            csv_writer.writerow([author] + confstat)
 
 if __name__ == '__main__':
     main(sys.argv)
