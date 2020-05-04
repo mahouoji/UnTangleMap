afe7e884c09f039fcd868d912d0ef9121602996a
Hang Dong <hang@Hangs-MBP.fios-router.home>
2020-05-04 15:45:28 -0400

    scatter plot matrix styles, add text on diagnal

--- src/js/scatterplotmatrix.js
+++ src/js/scatterplotmatrix.js
@@ -16,9 +16,19 @@ ScatterMatrix.prototype = {
     this.svg.selectAll("*").remove();
     for (let i = 0; i < this.dimDisplayed.length; i++) {
       for (let j = 0; j < this.dimDisplayed.length; j++) {
-        if (i === j) { continue; }
         let xLabel = this.dimDisplayed[i];
         let yLabel = this.dimDisplayed[j];
+        if (i === j) {
+          this.svg.append('g')
+            .attr("transform", `translate(${this.posScale(xLabel.name)},${this.posScale(yLabel.name)})`)
+            .append('text')
+            .attr("x", this.opt.scatterSize / 2)
+            .attr("y", this.opt.scatterSize / 2)
+            .text(xLabel.name)
+            .attr("text-anchor", "middle")
+            .style("font-size", "10px");
+          continue;
+        }
         // Add X Scale of each graph
         let xExtent = d3.extent(this.itemDisplayed, d=>+d[xLabel.index]);
         let xScale = d3.scaleLinear()
@@ -40,9 +50,9 @@ ScatterMatrix.prototype = {
         // Add X and Y axis in tmp
         tmp.append("g")
           .attr("transform", `translate(0,${this.opt.scatterSize - this.opt.scatterMargin * 2})`)
-          .call(d3.axisBottom(xScale).ticks(3));
+          .call(d3.axisBottom(xScale).ticks(2));
         tmp.append("g")
-          .call(d3.axisLeft(yScale).ticks(3));
+          .call(d3.axisLeft(yScale).ticks(2));
         // Add circle
         tmp.selectAll('circle')
           .data(this.itemDisplayed)
@@ -59,7 +69,7 @@ ScatterMatrix.prototype = {
   },
   updateLabelSelected: function(labelsSelected) {
     this.dimDisplayed = Object.values(labelsSelected);
-    if (this.dimDisplayed.length <= 0) { return; }
+    if (this.dimDisplayed.length === 0) { this.svg.selectAll("*").remove(); return; }
     this.opt.scatterSize = this.opt.inWidth / this.dimDisplayed.length;
     console.log(this.opt.scatterSize);
     this.posScale = d3.scalePoint()
@@ -71,7 +81,7 @@ ScatterMatrix.prototype = {
   initData: function(data) {
     this.data = data;
     this.dimensions = data.labels;
-    this.dimDisplayed = this.dimensions.map((d, i)=>{return {name: d.name, index: i};}).slice(0, 3);
+    this.dimDisplayed = [];//this.dimensions.map((d, i)=>{return {name: d.name, index: i};}).slice(0, 3);
     this.itemDisplayed = data.items.map(item=>item.normVec);
     this.updateLabelSelected(this.dimDisplayed);
   }
@@ -108,155 +118,3 @@ ScatterMatrix.init = function(selector, userOpt) {
 ScatterMatrix.init.prototype = ScatterMatrix.prototype;
 global.ScatterMatrix = ScatterMatrix;
 }(window, d3));
-
-/*
-// Dimension of the whole chart. Only one size since it has to be square
-var marginWhole = {top: 10, right: 10, bottom: 10, left: 10},
-    sizeWhole = 640 - marginWhole.left - marginWhole.right
-
-// Create the svg area
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", sizeWhole  + marginWhole.left + marginWhole.right)
-    .attr("height", sizeWhole  + marginWhole.top + marginWhole.bottom)
-  .append("g")
-    .attr("transform", "translate(" + marginWhole.left + "," + marginWhole.top + ")");
-
-
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) {
-
-  // What are the numeric variables in this dataset? How many do I have
-  var allVar = ["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]
-  var numVar = allVar.length
-
-  // Now I can compute the size of a single chart
-  mar = 20
-  size = sizeWhole / numVar
-
-
-  // ----------------- //
-  // Scales
-  // ----------------- //
-
-  // Create a scale: gives the position of each pair each variable
-  var position = d3.scalePoint()
-    .domain(allVar)
-    .range([0, sizeWhole-size])
-
-  // Color scale: give me a specie name, I return a color
-  var color = d3.scaleOrdinal()
-    .domain(["setosa", "versicolor", "virginica" ])
-    .range([ "#402D54", "#D18975", "#8FD175"])
-
-
-  // ------------------------------- //
-  // Add charts
-  // ------------------------------- //
-  for (i in allVar){
-    for (j in allVar){
-
-      // Get current variable name
-      var var1 = allVar[i]
-      var var2 = allVar[j]
-
-      // If var1 == var2 i'm on the diagonal, I skip that
-      if (var1 === var2) { continue; }
-
-      // Add X Scale of each graph
-      xextent = d3.extent(data, function(d) { return +d[var1] })
-      var x = d3.scaleLinear()
-        .domain(xextent).nice()
-        .range([ 0, size-2*mar ]);
-
-      // Add Y Scale of each graph
-      yextent = d3.extent(data, function(d) { return +d[var2] })
-      var y = d3.scaleLinear()
-        .domain(yextent).nice()
-        .range([ size-2*mar, 0 ]);
-
-      // Add a 'g' at the right position
-      var tmp = svg
-        .append('g')
-        .attr("transform", "translate(" + (position(var1)+mar) + "," + (position(var2)+mar) + ")");
-
-      // Add X and Y axis in tmp
-      tmp.append("g")
-        .attr("transform", "translate(" + 0 + "," + (size-mar*2) + ")")
-        .call(d3.axisBottom(x).ticks(3));
-      tmp.append("g")
-        .call(d3.axisLeft(y).ticks(3));
-
-      // Add circle
-      tmp
-        .selectAll("myCircles")
-        .data(data)
-        .enter()
-        .append("circle")
-          .attr("cx", function(d){ return x(+d[var1]) })
-          .attr("cy", function(d){ return y(+d[var2]) })
-          .attr("r", 3)
-          .attr("fill", function(d){ return color(d.Species)})
-    }
-  }
-
-
-  // ------------------------------- //
-  // Add histograms = diagonal
-  // ------------------------------- //
-  for (i in allVar){
-    for (j in allVar){
-
-      // variable names
-      var var1 = allVar[i]
-      var var2 = allVar[j]
-
-      // If var1 == var2 i'm on the diagonal, otherwisee I skip
-      if (i != j) { continue; }
-
-      // create X Scale
-      xextent = d3.extent(data, function(d) { return +d[var1] })
-      var x = d3.scaleLinear()
-        .domain(xextent).nice()
-        .range([ 0, size-2*mar ]);
-
-      // Add a 'g' at the right position
-      var tmp = svg
-        .append('g')
-        .attr("transform", "translate(" + (position(var1)+mar) + "," + (position(var2)+mar) + ")");
-
-      // Add x axis
-      tmp.append("g")
-        .attr("transform", "translate(" + 0 + "," + (size-mar*2) + ")")
-        .call(d3.axisBottom(x).ticks(3));
-
-      // set the parameters for the histogram
-       var histogram = d3.histogram()
-           .value(function(d) { return +d[var1]; })   // I need to give the vector of value
-           .domain(x.domain())  // then the domain of the graphic
-           .thresholds(x.ticks(15)); // then the numbers of bins
-
-       // And apply this function to data to get the bins
-       var bins = histogram(data);
-
-       // Y axis: scale and draw:
-       var y = d3.scaleLinear()
-            .range([ size-2*mar, 0 ])
-            .domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-
-       // append the bar rectangles to the svg element
-       tmp.append('g')
-          .selectAll("rect")
-          .data(bins)
-          .enter()
-          .append("rect")
-             .attr("x", 1)
-             .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-             .attr("width", function(d) { return x(d.x1) - x(d.x0)  ; })
-             .attr("height", function(d) { return (size-2*mar) - y(d.length); })
-             .style("fill", "#b8b8b8")
-             .attr("stroke", "white")
-    }
-  }
-
-
-})*/
